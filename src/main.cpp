b7979c46aa48df62c263eb4754b8420497e06508
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-01-13 19:02:15 +0100

    create header

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
+#include <fstream>
 #include <iostream>
+
+#define TYPE "int"
+
+std::filesystem::path path(const std::string& filename, const std::string& type)
+{
+    const std::filesystem::path out_dir = "./vector_src";
+    const std::string filename_full = type + "_" + filename;
+
+    std::filesystem::path out = out_dir;
+    out.append(filename_full);
+    return out;
+}
 
 int main()
 {
-    std::cout << "yo\n";
+    std::filesystem::path header = path("vector.h", TYPE);
+
+    std::ofstream myfile;
+    myfile.open(header);
+    myfile << "#include <stdio.h>\n";
+    myfile.close();
 }
